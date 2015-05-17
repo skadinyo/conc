be512277357431bc7718b94f727bec149378c659
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-05-17 10:04:58 +0700

    problem 12

--- src/conc/alpha.clj
+++ src/conc/alpha.clj
@@ -118,9 +118,9 @@
   [lim]
   (let [sum (*' lim (/ (inc lim) 2))]
     (- (* sum sum)
-        (* (+ (* 2 lim) 1)
-            (inc lim)
-            (/ lim 6)))))
+       (* (+ (* 2 lim) 1)
+          (inc lim)
+          (/ lim 6)))))
 
 
 ;;problem 7
@@ -164,7 +164,7 @@
 
 ;;problem 9
 
-(defn eul-9-1
+(defn eul-9-1®
   [n]
   (for [b (range 4 n)
         c (range 3 n)
@@ -182,3 +182,115 @@
   (reduce +' (m/primes-to lim)))
 
 ;;avg 100 ms
+
+;;problem 11
+
+(defn eul-11
+  []
+  (->> (slurp "./resources/problem-11.txt")
+       (clojure.string/split-lines)
+       (mapv #(mapv (fn [a]
+                      (Integer/parseInt a))
+                    (map str
+                         (clojure.string/split % #" "))))))
+
+(defn eul-11-1
+  []
+  (let [refs (eul-11)
+        h1 (fn [i j]
+             (map #(get-in refs [i %])
+                  (range j (+ j 4))))
+        h2 (fn [i j]
+             (map #(get-in refs [(+ i 3) %])
+                  (range j (+ j 4))))
+        v1 (fn [i j]
+             (map #(get-in refs [% j])
+                  (range i (+ i 4))))
+        v2 (fn [i j]
+             (map #(get-in refs [% (+ j 3)])
+                  (range i (+ i 4))))
+        d1 (fn [i j]
+             (map #(get-in refs [%1 %2])
+                  (range i (+ i 4))
+                  (range j (+ j 4))))
+        d2 (fn [i j]
+             (map #(get-in refs [%1 %2])
+                  (range (+ 3 i) (dec i) -1)
+                  (range j (+ j 4))))
+        call (fn [i j]
+               (map (fn [f]
+                      (f i j))
+                    [h1 h2 v1 v2 d1 d2]))]
+    (->> (for [i (range 0 (- (count refs) 4))
+               j (range 0 (- (count refs) 4))]
+           (map #(reduce * %)
+                (call i j)))
+         (flatten)
+         (apply max))))
+
+;;average 16 ms
+
+;;problem 12
+
+(defn gen-tri
+  "generate triangle number"
+  ([] (gen-tri 1))
+  ([x]
+   (cons (reduce + (range 1 (inc x))) (lazy-seq (gen-tri (inc x))))))
+
+(defn eul-12-1
+  [n]
+  (let [primes (m/primes-to 100000)
+        lim (last primes)
+        temp (into-array (repeat lim {}))
+        update-array (fn [p]
+                       (doseq [i (range p lim p)]
+                         (aset temp i
+                               (assoc
+                                 (aget temp i)
+                                 p (count-div p i)))))]
+    (do
+      (doseq [p primes]
+        (update-array p))
+      (let [refs (int-array (map #(let [v (vals %)]
+                                   (if (nil? v)
+                                     0
+                                     (apply + (conj v (count v)))))
+                                 temp))]
+        (loop [[x & xs] (take-while #(< % lim) (gen-tri))]
+          (if (nil? x)
+            false
+            (if (>= (aget refs x) n)
+              x
+              (recur xs))))))))
+
+;;sieve not done
+
+(defn prime-fac
+  [n]
+  (let [p (m/primes-to (inc n))]
+    (if (empty? p)
+      {}
+      (->> p
+           (filter #(zero? (rem n %)))
+           (map (fn [i]
+                  {i (count-div i n)}))
+           (reduce #(merge %1 %2))))))
+
+(defn eul-12-2
+  [n]
+  (loop [i 3 tri 3]
+    (do
+      (println i " " tri " " (prime-fac tri))
+      (if (>= (let [v (vals (prime-fac tri))]
+                (apply + (conj v (count v))))
+              n)
+        tri
+        (recur (inc i)
+               (reduce + (range i 0 -1)))
+        ))))
+
+;;too slow
+
+;;problem 13
+
