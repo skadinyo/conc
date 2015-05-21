de190eeccd0302785813781279aef5caf1aed8f5
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-05-22 03:38:47 +0700

    for clojure reduction

---
+++ src/conc/for.clj
@@ -0,0 +1,20 @@
+(ns conc.for)
+
+(defn one?
+  [n]
+  (or (= 1 n)
+    (= "1" n)))
+
+(defn reduce-
+  ([f [x & xs]] (reduce- f x xs))
+  ([f init args]
+    (if (or (nil? args) (empty? args))
+      init
+      (reduce- f (f init (first args)) (rest args)))))
+
+(fn reductions-
+  ([f [x & xs]] (reductions- f x xs))
+  ([f init [x & xs]]
+    (if x
+      (cons init (lazy-seq (reductions- f (f init x) xs)))
+      [init])))
